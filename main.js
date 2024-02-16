a72cb2844068de21b06fb10fb5cb1a7cbf80c6e9
Urmil Bhavsar <urmilbhavsar8@gmail.com>
2024-02-16 12:40:53 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,72 @@
+import * as THREE from "three";
+import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gasp from "gasp";
+
+//Scene
+const scene = new THREE.Scene();
+
+//Create a cube
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial();
+const mesh = new THREE.Mesh(geometry, material);
+mesh.material.color =  {r: 0, g: 255, b: 131,}
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//Light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGL1Renderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene, camera);
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+//Resize
+window.addEventListener("resize", () => {
+  //Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update camera
+  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop();
+
+const tl = gasp
